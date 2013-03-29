53fcb7e6ad349011be0754d66d54c4ea727fea2d
Stefan Rothe <info@stefan-rothe.ch>
2013-03-29 11:37:11 +0100

    Fixed bug in Java implementation of log window.
    Java platform implementation changes:
    
      * Jeda terminates correclty when log window is closed using the close button.

--- jeda/src/ch/jeda/platform/java/LogWindow.java
+++ jeda/src/ch/jeda/platform/java/LogWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 by Stefan Rothe
+ * Copyright (C) 2012 - 2013 by Stefan Rothe
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -79,7 +79,7 @@ class LogWindow extends BaseWindow {
     }// </editor-fold>//GEN-END:initComponents
 
     private void closeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeButtonActionPerformed
-        this.dispose();
+        this.cancel();
     }//GEN-LAST:event_closeButtonActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton closeButton;
