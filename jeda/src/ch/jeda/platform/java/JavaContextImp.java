f3c37b2bb2c581f8db0561da79ae67c44d37faf5
Stefan Rothe <info@stefan-rothe.ch>
2013-04-29 19:28:17 +0200

    Removed code loading JInput native libs from Jeda.

--- jeda/src/ch/jeda/platform/java/JavaContextImp.java
+++ jeda/src/ch/jeda/platform/java/JavaContextImp.java
@@ -84,36 +84,6 @@ class JavaContextImp implements ContextImp {
         setLookAndFeel();
         this.resourceFinder = new ResourceFinder();
         this.windowManager = new WindowManager();
-        loadNativeLibrary("jinput-dx8.dll");
-        loadNativeLibrary("jinput-dx8_64.dll");
-        loadNativeLibrary("jinput-raw_64.dll");
-        loadNativeLibrary("jinput-raw.dll");
-        loadNativeLibrary("jinput-wintab.dll");
-        loadNativeLibrary("libjinput-linux.so");
-        loadNativeLibrary("libjinput-linux64.so");
-        loadNativeLibrary("libjinput-osx.jnilib");
-    }
-
-    private static void loadNativeLibrary(String libraryName) {
-        final InputStream inputStream = JavaContextImp.class.getClassLoader().getResourceAsStream(libraryName);
-        try {
-            final String tempDir = System.getProperty("java.io.tmpdir");
-            System.setProperty("net.java.games.input.librarypath", tempDir);
-            final File libraryFile = new File(tempDir, libraryName);
-            libraryFile.deleteOnExit();
-            FileOutputStream fileOutputStream = new FileOutputStream(libraryFile);
-            final byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) > 0) {
-                fileOutputStream.write(buffer, 0, bytesRead);
-            }
-
-            fileOutputStream.close();
-            inputStream.close();
-        }
-        catch (Exception ex) {
-            //ex.printStackTrace();
-        }
     }
 
     private static void setLookAndFeel() {
