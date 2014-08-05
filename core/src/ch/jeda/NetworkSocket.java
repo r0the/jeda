270bfd6b07c23ff31b89ec70c1ecf57c0f63d087
Stefan Rothe <info@stefan-rothe.ch>
2014-08-05 19:03:25 +0200

    Added network support:
      - Added NetworkSocket class for text-based socket communication
      - Added NetworkServer implementing a simple socket server.

---
+++ core/src/ch/jeda/NetworkSocket.java
@@ -0,0 +1,256 @@
+/*
+ * Copyright (C) 2014 by Stefan Rothe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY); without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.jeda;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * Represents an endpoint for a network connection. A socket can be connected to a remote socket. This class is
+ * thread-safe.
+ *
+ * @since 1.2
+ */
+public final class NetworkSocket {
+
+    private final Charset charset;
+    private final Object lock;
+    private BufferedReader in;
+    private PrintWriter out;
+    private String remoteAddress;
+    private Socket socket;
+
+    /**
+     * Constructs a new network socket.
+     *
+     * @since 1.2
+     */
+    public NetworkSocket() {
+        this.charset = Charset.forName("UTF-8");
+        this.lock = new Object();
+    }
+
+    NetworkSocket(final Socket socket) {
+        this();
+        this.init(socket);
+    }
+
+    /**
+     * Tries to open a connection to the specified server.
+     *
+     * @param hostName the host name of the server
+     * @param port the port of the server
+     *
+     * @since 1.2
+     */
+    public boolean connect(final String hostName, final int port) {
+        this.disconnect();
+
+        final Socket newSocket = new Socket();
+        try {
+            newSocket.connect(new InetSocketAddress(hostName, port));
+            this.init(newSocket);
+        }
+        catch (final IOException ex) {
+            this.disconnect();
+        }
+
+        return this.isConnected();
+    }
+
+    /**
+     * Closes an existing connection. Has no effect if the socket has no open connection.
+     *
+     * @since 1.2
+     */
+    public void disconnect() {
+        synchronized (this.lock) {
+            if (this.socket != null) {
+                try {
+                    this.socket.close();
+                }
+                catch (final IOException ex) {
+                    // ignore
+                }
+
+                this.socket = null;
+                this.in = null;
+                this.out = null;
+            }
+        }
+    }
+
+    /**
+     * Checks if the socket has an active connection.
+     *
+     * @return <tt>true</tt> if the socket has an active connection, otherwise <tt>false</tt
+     *
+     * @since 1.2
+     */
+    public boolean isConnected() {
+        synchronized (this.lock) {
+            return this.socket != null && !this.socket.isClosed();
+        }
+    }
+
+    /**
+     * Checks if the socket has at least one data object to receive.
+     *
+     * @return <tt>true</tt> if the socket has at least one data object to receive, otherwise <tt>false</tt>
+     *
+     * @see #receiveData()
+     * @since 1.2
+     */
+    public boolean hasData() {
+        return this.hasLine();
+    }
+
+    /**
+     * Checks if the socket has at least one line of text to receive.
+     *
+     * @return <tt>true</tt> if the socket has at least one line of text to receive, otherwise <tt>false</tt>
+     *
+     * @see #receiveLine()
+     * @since 1.2
+     */
+    public boolean hasLine() {
+        synchronized (this.lock) {
+            if (this.in == null) {
+                return false;
+            }
+
+            try {
+                return this.in.ready();
+            }
+            catch (final IOException ex) {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Returns a unique address of the remote endpoint of the connection. The address consists of the remote endpoints
+     * IP address followed by the port number. Returns <tt>null</tt> if no connection has yet been established.
+     *
+     * @return a unique address of the remote endpoint of the connection
+     *
+     * @since 1.2
+     */
+    public String getRemoteAddress() {
+        synchronized (this.lock) {
+            return this.remoteAddress;
+        }
+    }
+
+    /**
+     * Receives a data object.
+     *
+     * @return a data object
+     *
+     * @since 1.2
+     */
+    public Data receiveData() {
+        final Data result = new Data();
+        synchronized (this.lock) {
+            if (this.in != null) {
+                try {
+                    result.loadFromLine(this.in.readLine());
+                }
+                catch (final IOException ex) {
+                    // ignore
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Receives a line of text. Returns <tt>null</tt> if no line can be received.
+     *
+     * @return a line of text or <tt>null</tt>
+     *
+     * @see #hasLine()
+     * @since 1.2
+     */
+    public String receiveLine() {
+        synchronized (this.lock) {
+            if (this.in == null) {
+                return null;
+            }
+
+            try {
+                return this.in.readLine();
+            }
+            catch (final IOException ex) {
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Sends a data object to the remote endpoint. Has no effect if the socket is not connected.
+     *
+     * @param data the data object to send
+     *
+     * @since 1.2
+     */
+    public void sendData(final Data data) {
+        synchronized (this.lock) {
+            if (this.out != null) {
+                this.out.println(data.toLine());
+                this.out.flush();
+            }
+        }
+    }
+
+    /**
+     * Sends a line of text to the remote endpoint. Has no effect if the socket is not connected.
+     *
+     * @param line the line of text to send
+     *
+     * @since 1.2
+     */
+    public void sendLine(final String line) {
+        synchronized (this.lock) {
+            if (this.out != null) {
+                this.out.println(line);
+                this.out.flush();
+            }
+        }
+    }
+
+    private void init(final Socket socket) {
+        try {
+            synchronized (this.lock) {
+                this.socket = socket;
+                this.in = new BufferedReader(new InputStreamReader(this.socket.getInputStream(), this.charset));
+                this.out = new PrintWriter(new OutputStreamWriter(this.socket.getOutputStream(), this.charset));
+                this.remoteAddress = Convert.toString(this.socket.getInetAddress().getHostAddress(), this.socket.getPort());
+            }
+        }
+        catch (final IOException ex) {
+            this.disconnect();
+        }
+    }
+}
---
+++ tests-java/src/ch/jeda/test/NetworkTest.java
@@ -0,0 +1,34 @@
+package ch.jeda.test;
+
+import ch.jeda.*;
+import ch.jeda.event.*;
+import ch.jeda.ui.*;
+
+public class NetworkTest extends Program implements TickListener {
+
+    NetworkSocket socket;
+
+    @Override
+    public void run() {
+        socket = new NetworkSocket();
+        socket.connect("jeda.ch", 80);
+        if (socket.isConnected()) {
+            writeLines("Verbunden");
+            socket.sendLine("GET /wiki/start HTTP/1.1");
+            socket.sendLine("Host: jeda.ch");
+            socket.sendLine("");
+        }
+        else {
+            writeLines("Verbindung konnte nicht hergestellt werden.");
+        }
+
+        Jeda.addTickListener(this);
+    }
+
+    @Override
+    public void onTick(final TickEvent event) {
+        while (socket.hasLine()) {
+            writeLines(socket.receiveLine());
+        }
+    }
+}
