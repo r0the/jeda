52922a6371f3a82dcb080a239697f50413b58208
Stefan Rothe <info@stefan-rothe.ch>
2014-08-15 18:28:31 +0200

    More network tests.

--- tests-java/src/ch/jeda/test/NetworkServer1Test.java
+++ tests-java/src/ch/jeda/test/NetworkServer1Test.java
@@ -4,13 +4,14 @@ import ch.jeda.*;
 import ch.jeda.event.*;
 import ch.jeda.ui.*;
 
-public class NetworkServerTest extends Program implements TickListener {
+public class NetworkServer1Test extends Program implements TickListener {
 
     NetworkServer server;
     NetworkSocket socket;
 
     @Override
     public void run() {
+        socket = new NetworkSocket();
         server = new NetworkServer();
         if (server.start(1248)) {
             writeLines("Server gestartet...");
@@ -24,16 +25,15 @@ public class NetworkServerTest extends Program implements TickListener {
 
     @Override
     public void onTick(final TickEvent event) {
-        if (socket == null) {
-            if (server.hasNewConnection()) {
-                socket = server.acceptNewConnection();
-            }
-        }
-        else {
+        if (socket.isConnected()) {
             if (socket.hasLine()) {
                 String line = socket.receiveLine();
                 socket.sendLine("Echo: " + line);
             }
+        }
+        else if (server.hasNewConnection()) {
+            writeLines("Akzeptiere Verbindung...");
+            socket = server.acceptNewConnection();
         }
     }
 }
