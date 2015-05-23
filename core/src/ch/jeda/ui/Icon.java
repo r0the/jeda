030ef8a41b5386d9ab679e198b485b59f895651e
Stefan Rothe <info@stefan-rothe.ch>
2015-05-23 09:22:56 +0200

    Added more icons.

--- core/src/ch/jeda/ui/Icon.java
+++ core/src/ch/jeda/ui/Icon.java
@@ -16,23 +16,296 @@
  */
 package ch.jeda.ui;
 
+/**
+ * Constants for predefined icons.
+ *
+ * @since 2.0
+ */
 public enum Icon {
 
-    ADD, ALARM, ANDROID, ARROW_LEFT, ARROW_RIGHT,
-    BED, BELL, BRIEFCASE, BRUSH, BUG,
-    CAKE, CHECK, CHEVRON_DOWN, CHEVRON_LEFT, CHEVRON_RIGHT, CHEVRON_UP, CLOSE, CLOUD, COPY, CUT,
-    EAT, EDUCATION, EYE,
-    FACE, FIRE, FLAG, FLARE, FLASH, FLOWER,
-    GLASS,
-    HEALING, HEART, HOME, HUMAN,
+    /**
+     * An alarm clock.
+     *
+     * @since 2.0
+     */
+    ALARM,
+    /**
+     * The Android robot.
+     *
+     * @since 2.0
+     */
+    ANDROID,
+    /**
+     * An arrow pointing to the left.
+     *
+     * @since 2.0
+     */
+    ARROW_LEFT,
+    /**
+     * An arrow pointing to the right.
+     *
+     * @since 2.0
+     */
+    ARROW_RIGHT,
+    /**
+     * A bell.
+     *
+     * @since 2.0
+     */
+    BELL,
+    /**
+     * A briefcase.
+     *
+     * @since 2.0
+     */
+    BRIEFCASE,
+    /**
+     * A broadcast icon.
+     *
+     * @since 2.0
+     */
+    BROADCAST,
+    /**
+     * A brush.
+     *
+     * @since 2.0
+     */
+    BRUSH,
+    /**
+     * A bug.
+     *
+     * @since 2.0
+     */
+    BUG,
+    /**
+     * A cake with a candle. The cake is a lie!
+     *
+     * @since 2.0
+     */
+    CAKE,
+    /**
+     * A photo camera.
+     *
+     * @since 2.0
+     */
+    CAMERA,
+    /**
+     * A check mark.
+     *
+     * @since 2.0
+     */
+    CHECK,
+    /**
+     * A chevron pointing down.
+     *
+     * @since 2.0
+     */
+    CHEVRON_DOWN,
+    /**
+     * A chevron pointing left.
+     *
+     * @since 2.0
+     */
+    CHEVRON_LEFT,
+    /**
+     * A chevron pointing right.
+     *
+     * @since 2.0
+     */
+    CHEVRON_RIGHT,
+    /**
+     * A chevron pointing up.
+     *
+     * @since 2.0
+     */
+    CHEVRON_UP,
+    /**
+     * A computer chip.
+     *
+     * @since 2.0
+     */
+    CHIP,
+    /**
+     * A circle.
+     *
+     * @since 2.0
+     */
+    CIRCLE,
+    /**
+     * A filled circle.
+     *
+     * @since 2.0
+     */
+    CIRCLE_FILLED,
+    /**
+     * A city.
+     *
+     * @since 2.0
+     */
+    CITY,
+    /**
+     * The icon for the standard close action.sy
+     *
+     * @since 2.0
+     */
+    CLOSE,
+    /**
+     * A cloud.
+     *
+     * @since 2.0
+     */
+    CLOUD,
+    /**
+     * A compass.
+     *
+     * @since 2.0
+     */
+    COMPASS,
+    /**
+     * The icon for the standard copy action.
+     *
+     * @since 2.0
+     */
+    COPY,
+    /**
+     * The icon for the standard cut action.
+     *
+     * @since 2.0
+     */
+    CUT,
+    /**
+     * A crossed knife and spoon.
+     *
+     * @since 2.0
+     */
+    EAT,
+    /**
+     * The icon for the standard delete action.
+     *
+     * @since 2.0
+     */
+    DELETE,
+    EDUCATION,
+    /**
+     * An eye.
+     *
+     * @since 2.0
+     */
+    EYE,
+    /**
+     * A face.
+     *
+     * @since 2.0
+     */
+    FACE,
+    /**
+     * A flame.
+     *
+     * @since 2.0
+     */
+    FIRE,
+    /**
+     * A flag.
+     *
+     * @since 2.0
+     */
+    FLAG, FLARE, FLASH, FLOWER, FOLDER,
+    GAMEPAD, GESTURE, GLASS, GPS_OFF, GPS_FIXED, GPS_NOT_FIXED, GRID,
+    HEAL,
+    /**
+     * An ear hearing sound.
+     *
+     * @since 2.0
+     */
+    HEAR,
+    /**
+     * A heart.
+     *
+     * @since 2.0
+     */
+    HEART, HOME,
+    /**
+     * A human.
+     *
+     * @since 2.0
+     */
+    HUMAN,
+    /**
+     * A key.
+     *
+     * @since 2.0
+     */
     KEY,
-    LANDSCAPE, LOCATION,
-    MAIL, MENU, MESSAGE_LEFT, MESSAGE_RIGHT, MONEY, MOON_1, MOON_2, MOON_3, MUG,
-    PASTE, PAUSE, PEN, PEOPLE, PERSON, PERSON_ADD, PLAY, PUZZLE,
-    RAINBOW, REDO, REFRESH, REMOVE,
-    SAVE, SETTINGS, SHIELD, STAR, STOP, SUN,
-    THUMB_DOWN, THUMB_UP, TIME,
+    LANDSCAPE, LETTER, LINK, LOCATION, LOCK,
+    MEASURE, MENU, MESSAGE_LEFT, MESSAGE_RIGHT, MINUS, MONEY, MOON, MUG, MUSIC_NOTE,
+    PALETTE, PASTE, PAUSE, PEN, PEOPLE, PERSON, PERSON_ADD, PLAY, PLUGS,
+    /**
+     * A plus sign.
+     *
+     * @since 2.0
+     */
+    PLUS,
+    /**
+     * A puzzle piece.
+     *
+     * @since 2.0
+     */
+    PUZZLE,
+    /**
+     * A rainbow.
+     *
+     * @since 2.0
+     */
+    RAINBOW, RECTANGLE, REDO, REFRESH,
+    SAVE, SEARCH, SETTINGS, SHIELD, SHUTTER,
+    /**
+     * A person sleeping in a bed.
+     *
+     * @since 2.0
+     */
+    SLEEP,
+    /**
+     * A square.
+     *
+     * @since 2.0
+     */
+    SQUARE,
+    /**
+     * A star.
+     *
+     * @since 2.0
+     */
+    STAR,
+    /**
+     * A square symbolizing the stop media operation.
+     *
+     * @since 2.0
+     */
+    STOP,
+    /**
+     * The sun.
+     *
+     * @since 2.0
+     */
+    SUN,
+    /**
+     * A hand with a thumb pointing down.
+     *
+     * @since 2.0
+     */
+    THUMB_DOWN,
+    /**
+     * A hand with a thumb pointing up.
+     *
+     * @since 2.0
+     */
+    THUMB_UP, TIME, TRIANGLE, TRIANGLE_FILLED,
     UNDO,
     VOLUME_DOWN, VOLUME_OFF, VOLUME_UP,
+    /**
+     * The world.
+     *
+     * @since 2.0
+     */
     WORLD
 }
